a9fd1fbe07ce8606e0b3fe2cdead94ffdf02786b
Moinul Islam <moinulmithu@hotmail.com>
2015-12-15 13:32:34 +0600

    Three new HTML page added

--- VTecFour_Four/app/app.config.js
+++ VTecFour_Four/app/app.config.js
@@ -10,6 +10,9 @@ app.config([
         $routeProvider
             .when('/', { templateUrl: 'app/views/home.tpl.html' })
             .when('/entry', { templateUrl: 'app/views/entry.tpl.html', controller: 'studentEntryController' })
-            .when('/list', { templateUrl: 'app/views/list.tpl.html', controller: 'studentListController' });
+            .when('/list', { templateUrl: 'app/views/list.tpl.html', controller: 'studentListController' })
+            .when('/DeptHome', { templateUrl: 'app/views/DepartmentHome.tpl.html' })
+            .when('/DeptEntry', { templateUrl: 'app/views/DepartmentEntry.tpl.html', controller: 'studentEntryController' })
+            .when('/DeptList', { templateUrl: 'app/views/DepartmentList.tpl.html', controller: 'studentListController' });
     }
 ]);
